f230924bf021652cbbcd7123878b5f1ea82ed828
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-08-03 12:31:37 -0400

    code ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import SearchForm from "./Components/SearchForm";
+import GifList from "./Components/GifList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query = "corgi") => {
+    axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=h6z3Ktcvw3UmZC5KZRCWwJziJ1qYKdUg`
+      )
+      .then((response) => {
+        this.setState({
+          gifs: response.data.data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch} />
+          </div>
+        </div>
+        <div className="main-content">
+          {this.state.loading ? (
+            <p className="red">loading... bandwidth what a drag.</p>
+          ) : (
+            <GifList data={this.state.gifs} />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
